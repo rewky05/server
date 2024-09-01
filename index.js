137f057379d3c58374e2ebb7abc2fadcd5632c46
rewky05 <apur.rosales.swu@phinmaed.com>
2024-09-01 22:24:10 +0800

    ohno

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const firstName = "April";
+const lastName = "Rosales";
+const age = 22;
+const schoolEmail = "apur.rosales.swu@phinmaed.com";
+
+validateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+app.get("/firstName", (req, res) => {
+  res.send(firstName.toUpperCase());
+});
+app.get("/lastName", (req, res) => {
+  res.send(lastName.toUpperCase());
+});
+app.get("/age", (req, res) => {
+  res.send(age.toString());
+});
+app.get("/schoolEmail", (req, res) => {
+  if (validateEmail.test(schoolEmail)) {
+    res.send(schoolEmail);
+  } else {
+    res.status(400).send("Invalid email");
+  }
+});
+
+app.get("/mydata", (req, res) => {
+  res.json({
+    first_name: firstName.toUpperCase(),
+    last_name: lastName.toUpperCase(),
+    age: age,
+    school_email: validateEmail.test(schoolEmail)
+      ? schoolEmail
+      : "Invalid email",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
